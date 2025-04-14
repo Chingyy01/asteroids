8258854be407b7f71ca90d73249a681628ac06d0
Chingyy01 <vincentmamone11@gmail.com>
2025-04-14 21:29:42 +1000

    end of day

--- main.py
+++ main.py
@@ -20,9 +20,5 @@ def main():
 
     pygame.quit()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 if __name__ == "__main__":
     main()
